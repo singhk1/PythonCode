ee46691ba7c3294c0604643d3df1fe5d4008f2f3
Kuldeep Singh <rekuldeep.singh@gmail.com>
2020-04-10 19:23:55 -0700

    added new file

---
+++ Python Code/DailyCoding/LongestConsecutiveSequence.py
@@ -0,0 +1,17 @@
+def longest_sequence(nums):
+    lo_seq = 0
+    my_set = set(nums)
+
+    for i in my_set:
+        if i - 1 not in my_set:
+            curr_num = i
+            curr_seq = 1
+            while curr_num + 1 in my_set:
+                curr_num += 1
+                curr_seq += 1
+            lo_seq = max(lo_seq, curr_seq)
+    return lo_seq
+
+
+nums = [100, 4, 200, 1, 3, 2]
+print(longest_sequence(nums))
