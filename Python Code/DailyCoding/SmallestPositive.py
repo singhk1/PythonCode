869915b479fe85f6d286a19de90fbe2f52bb8fea
Kuldeep Singh <rekuldeep.singh@gmail.com>
2020-05-16 16:50:49 -0700

    Added

---
+++ Python Code/DailyCoding/SmallestPositive.py
@@ -0,0 +1,17 @@
+'''
+Given a sorted array, find the smallest positive integer that is not the sum of a subset of the array.
+For example, for the input [1, 2, 3, 10], you should return 7.
+'''
+
+def smallest(nums):
+    res = 1
+    for i in range(len(nums)):
+        if nums[i] > res:
+            return res
+        else:
+            res = res + nums[i]
+    return res
+
+
+nums = [1, 2, 3, 10]
+print(smallest(nums))
